6c90b8bc48ef6712a9401a61d4d92c5c0a30c98d
viniandrs <viniciusandreossi@gmail.com>
2021-09-16 15:31:32 -0300

    Ready to compile. Start debugging

--- main.cpp
+++ main.cpp
@@ -6,31 +6,40 @@ using namespace std;
 #include "include/PrimaryGeneratorAction.h"
 
 #include "G4RunManager.hh"
-#include "G4VUserPhysicsList.hh"
-#include "G4VModularPhysicsList.hh"
+#include "G4ScoringManager.hh"
+
 #include "QBBC.hh"
+
+//Interface classes
 #include "G4UIExecutive.hh"
 #include "G4UImanager.hh"
-#define G4VIS_USE_OPENGLX
 #define G4VIS_USE_OPENGLQT
 #include "G4VisExecutive.hh"
 #include "G4VisManager.hh"
-#include "G4OpticalPhysics.hh"
 
 //Calling the User Action classes
 #include "include/RunAction.h"
 #include "include/EventAction.h"
 
+//Physics configuration
+#include "FTFP_BERT.hh"
+#include "G4OpticalPhysics.hh"
+#include "G4EmStandardPhysics_option2.hh"
+
 int main(int argc, char **argv)
 {
-    //Creating our run manager
     G4RunManager *runManager = new G4RunManager;
 
-    // Physics list
-    G4VModularPhysicsList *physicsList = new QBBC;
-    physicsList->SetVerboseLevel(1);
+    // Physics list setup
+    G4VModularPhysicsList *physicsList = new FTFP_BERT;
+    physicsList->ReplacePhysics(new G4EmStandardPhysics_option2());
 
+    G4OpticalPhysics *opticalPhysics = new G4OpticalPhysics();
+    opticalPhysics->Configure(kCerenkov, true);
+    opticalPhysics->Configure(kScintillation, true);
+    physicsList->DumpList();
     physicsList->RegisterPhysics(new G4OpticalPhysics());
+
     runManager->SetUserInitialization(physicsList);
 
     // User initialization classes
@@ -41,13 +50,14 @@ int main(int argc, char **argv)
     runManager->SetUserAction(new RunAction());
     runManager->SetUserAction(new PrimaryGeneratorAction());
     runManager->SetUserAction(new EventAction());
-    runManager->SetUserAction(/*stepping action*/)
 
     runManager->Initialize();
 
+    //Initializing the visualization manager
     auto *visManager = new G4VisExecutive();
     visManager->Initialize();
 
+    //User interface
     auto *uiExecutive = new G4UIExecutive(argc, argv, "Qt");
 
     if (argc == 1)
@@ -57,6 +67,8 @@ int main(int argc, char **argv)
     else
     {
         auto *uiManager = G4UImanager::GetUIpointer();
+        
+        //Executing instructions from a macro file
         uiManager->ApplyCommand("/control/execute " + G4String(argv[1]));
         uiExecutive->SessionStart();
     }
